fac77dcf9578c679ebe1f2ef0a6640030e0d9c1a
OgunmisiM <Ogunmisioluwafemi@gmail.com>
2022-08-16 17:29:42 +0100

    linkedin login clone

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 300) {
+          $(".navbar").css({
+              "box-shadow": "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+            });
+        }
+       
+        else{
+            $("nav.navbar").css("background" , "#e8e7ee");  	
+        }
+    })
+  })
